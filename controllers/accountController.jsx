cdacf90c7e90433b88e5336ff275dd323bfa46c5
ebubekeyz <ebubeofforjoe@gmail.com>
2024-06-12 20:26:15 +0530

    1

---
+++ controllers/accountController.jsx
@@ -0,0 +1,197 @@
+const Account = require('../models/Account');
+const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthorizedError } = require('../errors');
+
+const createAccount = async (req, res) => {
+  req.body.user = req.user.userId;
+  const account = await Account.create(req.body);
+  res.status(StatusCodes.CREATED).json({ attributes: account });
+};
+
+const getAllAccounts = async (req, res) => {
+  let { user, date, name, accountNumber } = req.query;
+
+  const queryObject = {
+    user: req.user.userId,
+  };
+
+  let result = Account.find(queryObject);
+
+  if (user) {
+    result = Account.find({ user: req.user.userId, user: { $eq: user } });
+  }
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt');
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt');
+  }
+
+  if (sort === 'a-z') {
+    result = result.sort('name');
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-name');
+  }
+
+  if (accountNumber) {
+    result = Account.find({
+      user: req.user.userId,
+      accountNumber: { $lte: accountNumber },
+    });
+  }
+
+  if (name) {
+    result = Account.find(queryObject, {
+      name: { $regex: name, $options: 'i' },
+    });
+  }
+  if (date) {
+    result = Account.find({
+      user: req.user.userId,
+      date: { $regex: date, $options: 'i' },
+    });
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const account = await result;
+
+  const totalAccount = await Account.countDocuments();
+  const numOfPage = Math.ceil(totalAccount / limit);
+
+  res.status(StatusCodes.OK).json({
+    account: account,
+    meta: {
+      pagination: { page: page, total: totalAccount, pageCount: numOfPage },
+    },
+  });
+};
+
+const getAccounts = async (req, res) => {
+  let { user, date, name, accountNumber, sort } = req.query;
+
+  let result = Account.find({});
+
+  if (user) {
+    result = Account.find({
+      user: { $eq: user },
+    });
+  }
+
+  if (sort === 'latest') {
+    result = result.sort('-createdAt');
+  }
+  if (sort === 'oldest') {
+    result = result.sort('createdAt');
+  }
+
+  if (sort === 'a-z') {
+    result = result.sort('name');
+  }
+  if (sort === 'z-a') {
+    result = result.sort('-name');
+  }
+
+  if (accountNumber) {
+    result = Account.find({ accountNumber: { $lte: accountNumber } });
+  }
+
+  if (name) {
+    result = Account.find({
+      name: { $regex: name, $options: 'i' },
+    });
+  }
+
+  if (date) {
+    result = Account.find({
+      date: { $regex: date, $options: 'i' },
+    });
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const account = await result;
+
+  const totalAccount = await Account.countDocuments();
+  const numOfPage = Math.ceil(totalAccount / limit);
+
+  res.status(StatusCodes.OK).json({
+    account: account,
+    meta: {
+      pagination: { page: page, total: totalAccount, pageCount: numOfPage },
+    },
+  });
+};
+
+const getSingleAccount = async (req, res) => {
+  const { id: accountId } = req.params;
+  const account = await Account.findOne({ _id: accountId });
+  if (!account) {
+    throw new BadRequestError(`Account with id ${accountId} does not exist`);
+  }
+
+  res.status(StatusCodes.OK).json({ Account: Account });
+};
+
+const editSingleAccount = async (req, res) => {
+  const { id: accountId } = req.params;
+  const account = await Account.findOneAndUpdate({ _id: accountId }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!account) {
+    throw new BadRequestError(`Account with id ${AccountId} does not exist`);
+  }
+  res.status(StatusCodes.OK).json({ account: account });
+};
+
+const editUserAccount = async (req, res) => {
+  const { id: userId } = req.params;
+  const account = await Account.updateMany({ user: userId }, req.body);
+
+  res.status(StatusCodes.OK).json({ account: account });
+};
+
+const deleteSingleAccount = async (req, res) => {
+  const { id: accountId } = req.params;
+  const account = await Account.findByIdAndDelete({ _id: accountId });
+  if (!account) {
+    throw new BadRequestError(`Account with id ${accountId} does not exist`);
+  }
+  res.status(StatusCodes.OK).json({ msg: 'Account Deleted' });
+};
+
+const deleteAllAccounts = async (req, res) => {
+  const account = await Account.deleteMany();
+  res.status(StatusCodes.OK).json({ msg: 'Account Deleted' });
+};
+
+const deleteUserAccount = async (req, res) => {
+  const { id: userId } = req.params;
+  const account = await Account.deleteMany({ user: userId });
+
+  res.status(StatusCodes.OK).json({ msg: 'Account successfully deleted' });
+};
+
+module.exports = {
+  getAccounts,
+  createAccount,
+  deleteUserAccount,
+  getAllAccounts,
+  getSingleAccount,
+  editSingleAccount,
+  deleteSingleAccount,
+  deleteAllAccounts,
+  editUserAccount,
+};
---
+++ models/Account.jsx
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const moment = require('moment');
+const validator = require('validator');
+
+const AccountSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    accountNumber: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: String,
+      default: moment().format('YYYY-DD-MM'),
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Account', AccountSchema);
