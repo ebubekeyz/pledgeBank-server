d6e11356b0e39f66b9ca6b24817801f09b3b68fa
ebubekeyz <ebubeofforjoe@gmail.com>
2024-06-16 18:38:07 +0100

    1

--- controllers/addFundController.js
+++ controllers/addFundController.js
@@ -128,7 +128,7 @@ const getAddFunds = async (req, res) => {
   }
 
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 1000;
   const skip = (page - 1) * limit;
 
   result = result.skip(skip).limit(limit);
--- controllers/withdrawController.js
+++ controllers/withdrawController.js
@@ -112,7 +112,7 @@ const getWithdraws = async (req, res) => {
   }
 
   const page = Number(req.query.page) || 1;
-  const limit = Number(req.query.limit) || 10;
+  const limit = Number(req.query.limit) || 1000;
   const skip = (page - 1) * limit;
 
   result = result.skip(skip).limit(limit);
